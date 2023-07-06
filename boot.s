a8d816acc3b956cbdbf531bac03bb5efeab9d315
Jules Elferink <42654259+juveron@users.noreply.github.com>
2023-07-06 12:35:57 +0200

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,8 +1,9 @@
 .set ALIGN,    1<<0
-.set MEMINFO,  1<<1      
+.set MEMINFO,  1<<1
 .set FLAGS,    ALIGN | MEMINFO
 .set MAGIC,    0x1BADB002
 .set CHECKSUM, -(MAGIC + FLAGS)
+
 .section .multiboot
 .align 4
 .long MAGIC
@@ -12,19 +13,100 @@
 .section .bss
 .align 16
 stack_bottom:
-.skip 16384 # 16 KiB
+.skip 16384 ; 16 KiB
 stack_top:
 
 .section .text
 .global _start
 .type _start, @function
 _start:
-	mov $stack_top, %esp
+    ; Set up the stack pointer
+    mov $stack_top, %esp
 
-	call kernel_main
- 
-	cli
-1:	hlt
-	jmp 1b
+    ; Call kernel_main
+    call kernel_main
+
+    ; Loop and halt
+    cli
+1:
+    hlt
+    jmp 1b
 
 .size _start, . - _start
+
+.section .text
+kernel_main:
+    ; Your kernel code goes here
+
+    ; Declare the GDT to the BIOS at address 0x00000800
+    .section .gdt, "awx", @nobits
+    .align 4
+gdt_start:
+    ; Null segment
+    dd 0
+    dd 0
+
+    ; Kernel Code segment
+    dw 0xFFFF ; Limit (16-bit)
+    dw 0x0000 ; Base (16-bit)
+    db 0x00   ; Base (8-bit)
+    db 0x9A   ; Access byte
+    db 0xCF   ; Granularity byte
+    db 0x00   ; Base (8-bit)
+
+    ; Kernel Data segment
+    dw 0xFFFF ; Limit (16-bit)
+    dw 0x0000 ; Base (16-bit)
+    db 0x00   ; Base (8-bit)
+    db 0x92   ; Access byte
+    db 0xCF   ; Granularity byte
+    db 0x00   ; Base (8-bit)
+
+    ; Kernel Stack segment
+    dw 0xFFFF ; Limit (16-bit)
+    dw 0x0000 ; Base (16-bit)
+    db 0x00   ; Base (8-bit)
+    db 0x92   ; Access byte
+    db 0xCF   ; Granularity byte
+    db 0x00   ; Base (8-bit)
+
+    ; User Code segment
+    dw 0xFFFF ; Limit (16-bit)
+    dw 0x0000 ; Base (16-bit)
+    db 0x00   ; Base (8-bit)
+    db 0x9A   ; Access byte
+    db 0xCF   ; Granularity byte
+    db 0x00   ; Base (8-bit)
+
+    ; User Data segment
+    dw 0xFFFF ; Limit (16-bit)
+    dw 0x0000 ; Base (16-bit)
+    db 0x00   ; Base (8-bit)
+    db 0x92   ; Access byte
+    db 0xCF   ; Granularity byte
+    db 0x00   ; Base (8-bit)
+
+    ; User Stack segment
+    dw 0xFFFF ; Limit (16-bit)
+    dw 0x0000 ; Base (16-bit)
+    db 0x00   ; Base (8-bit)
+    db 0x92   ; Access byte
+    db 0xCF   ; Granularity byte
+    db0x00   ; Base (8-bit)
+
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; Size of GDT
+    dd gdt_start              ; Base address of GDT
+
+    ; Declare the GDT to the BIOS at address 0x00000800
+    .section .bios_gdt, "awx", @progbits
+    .align 8
+    .long 0x00000800
+    .short 0x0000
+    .short 0xFFFF
+
+    ; Fill the remaining bytes with zeros until 0x00000800
+    .section .bios_padding, "awx", @nobits
+    .align 1
+    .skip 2048 - ($ - $$)
