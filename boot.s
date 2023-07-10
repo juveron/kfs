aa592e543349048c9931116b7353ca3f8d6bfd24
Jules Elferink <juveron@student.42.fr>
2023-07-10 16:42:01 +0200

    fix start

--- boot.s
+++ boot.s
@@ -1,4 +1,3 @@
-/* Declare constants for the multiboot header. */
 .set ALIGN,    1<<0
 .set MEMINFO,  1<<1
 .set FLAGS,    ALIGN | MEMINFO
@@ -11,19 +10,31 @@
 .long FLAGS
 .long CHECKSUM
 
+
 .section .bss
 .align 16
 stack_bottom:
 .skip 16384 # 16 KiB
 stack_top:
 
+
 .section .text
 .global _start
 .type _start, @function
 _start:
+
+
+
 	mov $stack_top, %esp
+
+
+
+
 	call kernel_main
+
 	cli
 1:	hlt
 	jmp 1b
+
+
 .size _start, . - _start
