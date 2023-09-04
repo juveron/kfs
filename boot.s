6694aa4cb1b1a13311477fe8ab56a2b059640625
Jules Elferink <juveron@student.42.fr>
2023-09-04 11:05:15 +0200

    add gdt into kernel, todo need print kernel

--- boot.s
+++ boot.s
@@ -1,107 +1,40 @@
-.set ALIGN,    1<<0 
-.set MEMINFO,  1<<1 
-.set FLAGS,    ALIGN | MEMINFO 
-.set MAGIC,    0x1BADB002 
-.set CHECKSUM, -(MAGIC + FLAGS) 
-  
-.section .multiboot 
-.align 4 
-.long MAGIC 
-.long FLAGS 
-.long CHECKSUM 
-  
-  
-.section .bss 
-.align 16 
-stack_bottom: 
-.skip 16384 # 16 KiB 
-stack_top: 
-  
-  
-.section .text 
-.global _start 
-.type _start, @function 
-_start: 
+.set ALIGN,    1<<0
+.set MEMINFO,  1<<1
+.set FLAGS,    ALIGN | MEMINFO
+.set MAGIC,    0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
 
 
-    mov $stack_top, %esp
-  
+.section .bss
+.align 16
+stack_bottom:
+.skip 16384 # 16 KiB
+stack_top:
 
-    call kernel_main
-  
-    cli 
-1:  
-    hlt 
-    jmp 1b 
-  
-  
-.size _start, . - _start
 
 .section .text
-kernel_main:
+.global _start
+.type _start, @function
+_start:
 
 
 
-    .section .gdt, "awx", @nobits
-    .align 4
-gdt_start:
+	mov $stack_top, %esp
 
-    dd 0
-    dd 0
 
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0x9A
-    db 0xCF
-    db 0x00
 
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0x92
-    db 0xCF
-    db 0x00
 
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0x92
-    db 0xCF
-    db 0x00
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0x9A
-    db 0xCF
-    db 0x00
+	call kernel_main
 
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0x92
-    db 0xCF
-    db 0x00
+	cli
+1:	hlt
+	jmp 1b
 
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 0x92
-    db 0xCF
-    db 0x00
 
-gdt_end:
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
-
-    .section .bios_gdt, "awx", @progbits
-    .align 8
-    .long 0x00000800
-    .short 0x0000
-    .short 0xFFFF
-
-    .section .bios_padding, "awx", @nobits
-    .align 1
-    .skip 2048 - ($ - $$)
-    
+.size _start, . - _start
