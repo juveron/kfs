2ab4032bf933a7a97a3fa1f0b301e8936657ca0b
Jules Elferink <juveron@student.42.fr>
2023-09-04 17:08:23 +0200

    add gdt

--- boot.s
+++ boot.s
@@ -1,40 +1,47 @@
-.set ALIGN,    1<<0
-.set MEMINFO,  1<<1
-.set FLAGS,    ALIGN | MEMINFO
-.set MAGIC,    0x1BADB002
-.set CHECKSUM, -(MAGIC + FLAGS)
-
-.section .multiboot
+.set ALIGN,    1<<0 
+.set MEMINFO,  1<<1 
+.set FLAGS,    ALIGN | MEMINFO 
+.set MAGIC,    0x1BADB002 
+.set CHECKSUM, -(MAGIC + FLAGS) 
+  
+.section .multiboot 
+.align 4 
+.long MAGIC 
+.long FLAGS 
+.long CHECKSUM 
+  
+.section .bss 
+.align 16 
+stack_bottom: 
+.skip 16384 # 16 KiB 
+stack_top: 
+  
+.section .data
 .align 4
-.long MAGIC
-.long FLAGS
-.long CHECKSUM
+gdt_start:
+    .quad 0x0  # Descriptor nul
+    .quad 0x00CF9A000000FFFF  # Descripteur de code
+    .quad 0x00CF92000000FFFF  # Descripteur de données
+gdt_end:
 
+.section .data
+gdt_descriptor:
+    .word gdt_end - gdt_start - 1
+    .long gdt_start
 
-.section .bss
-.align 16
-stack_bottom:
-.skip 16384 # 16 KiB
-stack_top:
+.section .text 
+.global _start 
+.type _start, @function 
+_start: 
+    mov $stack_top, %esp
 
+    lgdt gdt_descriptor
 
-.section .text
-.global _start
-.type _start, @function
-_start:
+    call kernel_main 
 
-
-
-	mov $stack_top, %esp
-
-
-
-
-	call kernel_main
-
-	cli
-1:	hlt
-	jmp 1b
-
+    cli 
+1: 
+    hlt 
+    jmp 1b 
 
 .size _start, . - _start
